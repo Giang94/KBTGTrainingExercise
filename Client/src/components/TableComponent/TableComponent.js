9118386e02b7adb11d6dba22466de14960cc51f5
Hieu Bui <bthieu@tma.com.vn>
2019-03-14 09:17:41 +0700

    Testing and implementing the Table component for Shopping Cart

--- Client/src/components/TableComponent/TableComponent.js
+++ Client/src/components/TableComponent/TableComponent.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import ReactDOM from 'react-dom';
 
 class TableComponent extends React.Component {
   render() {
     return (
-      <table className="order-list"></table>
+      <table className="table-component">
+        <thead>
+          <tr>
+            {this.props.columns.map((column, id)=>(
+              <th key={id}>{column.name}</th>
+            ))}
+          </tr>
+        </thead>
+        <tbody>
+          {this.props.data.map((d, i)=>(
+            <tr key={i}>
+              {this.props.columns.map((column, j)=>(
+                <td key={j}>{d[column.key]}</td>
+              ))}
+            </tr>
+          ))}
+        </tbody>
+      </table>
     );
   }
 }
+
+TableComponent.propTypes = {
+  columns: PropTypes.array,
+  data: PropTypes.array
+};
+
+TableComponent.defaultProps = {
+  columns: [
+    {name: '', key: ''}
+  ],
+  data: []
+};
 
 export default TableComponent;
